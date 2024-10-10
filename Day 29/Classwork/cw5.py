837b187f65066269829097c7a1cd9f5d3dc9128c
giorgi-gagoshidze <giorgigagoshidze20@gmail.com>
2024-10-10 12:38:48 -0700

    commit

---
+++ Day 29/Classwork/cw5.py
@@ -0,0 +1,5 @@
+
+hello=["wwjhaifhsiudfaidsuhfiusnfahfsagdufii","asjdhfkjdfhudjbafjhbdvhbasjbf","weugnaiufliadgsfijnasdi7fbuwdgfcu"]
+print(len(hello[0]))
+print(len(hello[1]))
+print(len(hello[2]))
