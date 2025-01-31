87ee40b2a5a10dd70e914cae5d64ba38ccfaf2be
giorgi-gagoshidze <giorgigagoshidze20@gmail.com>
2025-01-31 13:40:16 -0800

    commit

--- Day 29/Classwork/cw3.py
+++ Day 29/Classwork/cw3.py
@@ -1,2 +1,9 @@
 #შექმენით 2 ლისტი და ორივედან ამოშალეთ 2-2 ცვლადი pop ფუნქციის გამოყენებით
-list=[""]
+list=["hello" "bye","bye"]
+list2=["hello", "bye", "bye"]
+list.pop()
+list.pop()
+list2.pop()
+list2.pop()
+print(list)
+print(list2)
--- Day 29/Classwork/cw5.py
+++ Day 29/Classwork/cw5.py
@@ -1,4 +1,4 @@
-
+#შექმენით 3 ცვლადი და დაითვალეთ რამდენი სიმბოლოა თითოეულ ცვლადში
 hello=["wwjhaifhsiudfaidsuhfiusnfahfsagdufii","asjdhfkjdfhudjbafjhbdvhbasjbf","weugnaiufliadgsfijnasdi7fbuwdgfcu"]
 print(len(hello[0]))
 print(len(hello[1]))
